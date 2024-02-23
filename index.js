387b2e2105ba77e9f3894425f26169ab1eb56ef0
fORESTA1 <shadowhex12bgmonster99gothika@gmail.com>
2024-02-23 13:21:02 +0200

    .

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const sqlite3 = require('sqlite3').verbose();
 const express = require('express')
 const app = express()
 const port = 3000
@@ -40,6 +41,86 @@ app.get('/schedule', (req, res) => {
   var room = req.query.room;
 
   var schedule = findSchedule(dayOfWeek, room);
+
+  res.send(schedule);
+})
+
+
+function findScheduleDB(dayOfWeek, room) {
+  let db = new sqlite3.Database('./mydb.sqlite3', (err) => {
+    if (err) {
+      console.error('Грешка при отваряне на базата данни', err.message);
+    } else {
+      console.log('Успешно свързване с базата данни.');
+    }
+  });
+}
+
+//   function getAll() {
+//     db.all("SELECT * FROM contacts", [], (err, rows) => {
+//       if (err) {
+//         throw err;
+//       }
+//       rows.forEach((row) => {
+//         console.log(row.name, row.email);
+//       });
+//     });
+//   }
+// }
+
+
+// let db = new sqlite3.Database('./mydb.sqlite3', (err) => {
+//   if (err) {
+//     console.error('Грешка при отваряне на базата данни', err.message);
+//   } else {
+//     console.log('Успешно свързване с базата данни.');
+
+//     db.run(`CREATE TABLE IF NOT EXISTS contacts (
+//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         name TEXT NOT NULL,
+//         email TEXT NOT NULL UNIQUE
+//       )`, (err) => {
+//         if (err) {
+//           console.error('Грешка при създаването на таблицата', err.message);
+//         } else {
+//           console.log('Таблицата е успешно създадена или вече съществува.');
+
+//           getAll();
+
+//         //   const insertData = `INSERT INTO contacts (name, email) VALUES (?, ?)`;
+//         //   db.run(insertData, ["name", "...@email.com"], (err) => {
+//         //     if (err) {
+//         //       console.error('Грешка при вмъкването на данни', err.message);
+//         //     } else {
+//         //       console.log('Данните са успешно вмъкнати.');
+
+//         //       db.close((err) => {
+//         //         if (err) {
+//         //           console.error('Грешка при затварянето на връзката с базата данни', err.message);
+//         //         } else {
+//         //           console.log('Връзката с базата данни е затворена.');
+//         //         }
+//         //       });
+//         //     }
+//         //   });
+//         // }
+//       }}
+//     )
+//   }
+// });
+
+app.get('/schedule-DB', (req, res) => {
+  console.log(req.query.d, req.query.m, req.query.y);
+
+  var updatedMonth = req.query.m-1
+
+  var date = new Date(Date.UTC(req.query.y,updatedMonth,req.query.d))
+
+  var dayOfWeek = date.getDay();
+
+  var room = req.query.room;
+
+  var schedule = findScheduleDB(dayOfWeek, room);
 
   res.send(schedule);
 })
