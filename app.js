03923124fb93f437cfbca317642071b57a347e2f
fORESTA1 <shadowhex12bgmonster99gothika@gmail.com>
2024-05-31 13:54:36 +0300

    app.js rework

--- app.js
+++ app.js
@@ -1,86 +1,76 @@
 const sqlite3 = require('sqlite3').verbose();
 
+let db = new sqlite3.Database('./mydb.sqlite3', (err) => { 
+  if (err) { 
+    console.error('Грешка при отваряне на базата данни', err.message); 
+  } else { 
+    console.log('Успешно свързване с базата данни.'); 
+    
+    db.run(`CREATE TABLE IF NOT EXISTS contacts ( 
+      id INTEGER PRIMARY KEY AUTOINCREMENT, 
+      name TEXT NOT NULL, 
+      email TEXT NOT NULL UNIQUE 
+    )`, (err) => { 
+      if (err) { 
+        console.error('Грешка при създаването на таблицата', err.message); 
+      } else { 
+        console.log('Таблицата contacts е успешно създадена или вече съществува.'); 
+        
+        
+        db.run(`CREATE TABLE IF NOT EXISTS phones ( 
+          id INTEGER PRIMARY KEY AUTOINCREMENT, 
+          contact_id INTEGER, 
+          phone TEXT NOT NULL 
+        )`, (err) => { 
+          if (err) { 
+            console.error('Грешка при създаването на таблицата phones', err.message); 
+          } else { 
+            console.log('Таблицата phones е успешно създадена или вече съществува.'); 
 
-let db = new sqlite3.Database('./mydb.sqlite3', (err) => {
-  if (err) {
-    console.error('Грешка при отваряне на базата данни', err.message);
-  } else {
-    console.log('Успешно свързване с базата данни.');
+            db.run(`INSERT INTO contacts (name, email) VALUES (?, ?)`, ["name", "abc@email.com"], (err) => {
+              if (err) {
+                console.error('Грешка при вмъкването на данни', err.message);
+              } else {
+                console.log('Данните са успешно вмъкнати.');
+    
+                db.run(`INSERT INTO phones (contact_id, phone) VALUES (?, ?)`, [1, "+359435345345"], (err) => {
+                  if (err) {
+                    console.error('Грешка при вмъкването на данни', err.message);
+                  } else {
+                    console.log('Данните са успешно вмъкнати.');
+    
+                    db.run(`INSERT INTO phones (contact_id, phone) VALUES (?, ?)`, [2, "+134534554645"], (err) => {
+                      if (err) {
+                        console.error('Грешка при вмъкването на данни', err.message);
+                      } else {
+                        console.log('Данните са успешно вмъкнати.');
+    
+                        db.all("SELECT * FROM phones left join contacts ON contacts.id = phones.contact_id", [], (err, rows) => {
+                          if (err) {
+                            throw err;
+                          }
+                          rows.forEach((row) => {
+                            console.log(row.name, row.email, row.phone);
+                          });
+                  
+                          db.close((err) => {
+                            if (err) {
+                              console.error('Грешка при затварянето на връзката с базата данни', err.message);
+                            } else {
+                              console.log('Връзката с базата данни е затворена.');
+                            }
+                          });  
+                        });
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
+        }); 
 
-    db.run(`CREATE TABLE IF NOT EXISTS contacts (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE
-      )`, (err) => {
-        if (err) {
-          console.error('Грешка при създаването на таблицата', err.message);
-        } else {
-          console.log('Таблицата е успешно създадена или вече съществува.');
-
-          getAll();
-
-        //   const insertData = `INSERT INTO contacts (name, email) VALUES (?, ?)`;
-        //   db.run(insertData, ["name", "...@email.com"], (err) => {
-        //     if (err) {
-        //       console.error('Грешка при вмъкването на данни', err.message);
-        //     } else {
-        //       console.log('Данните са успешно вмъкнати.');
-
-        //       db.close((err) => {
-        //         if (err) {
-        //           console.error('Грешка при затварянето на връзката с базата данни', err.message);
-        //         } else {
-        //           console.log('Връзката с базата данни е затворена.');
-        //         }
-        //       });
-        //     }
-        //   });
-        // }
-      }}
-    )
-  }
+      } 
+    }); 
+  } 
 });
-
-
-
-
-
-
-function getAll() {
-  db.all("SELECT * FROM contacts", [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
-    rows.forEach((row) => {
-      console.log(row.name, row.email);
-    });
-  });
-}
-  
-
-//   db.get("SELECT * FROM contacts WHERE email = ?", [email], (err, row) => {
-//     if (err) {
-//       return console.error(err.message);
-//     }
-//     console.log(row.name, row.email);
-//   });
-  
-
-
-//   db.each("SELECT * FROM contacts", (err, row) => {
-//     if (err) {
-//       throw err;
-//     }
-//     console.log(row.name, row.email);
-//   });
-  
-
-
-//   db.close((err) => {
-//     if (err) {
-//       console.error(err.message);
-//     }
-//     console.log('Close the database connection.');
-//   });
-  
-  
--- storage.js
+++ storage.js
@@ -1,34 +1,34 @@
-const fs = reqire("fs");
+const fs = require("fs");
 const  path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 
-
-const db = new sqlite3.Database(':memory:');
+var dbname = 'C:\\Users\\Omega\\NP\\ScheduleServer\\Schedule-server-\\schedule.db';
+const db = new sqlite3.Database(dbname);
 
 
 db.serialize(() => {
   db.run("CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
 
 
-  db.run("INSERT INTO items (name) VALUES (?)", ["Item 1"]);
+  // db.run("INSERT INTO items (name) VALUES (?)", ["Item 1"]);
 
  
-  db.all("SELECT * FROM items", (err, rows) => {
-    if (err) {
-      console.error("Error selecting items:", err);
-    } else {
-      console.log("Items:");
-      rows.forEach((row) => {
-        console.log(row.id, row.name);
-      });
-    }
-  });
+  // db.all("SELECT * FROM items", (err, rows) => {
+  //   if (err) {
+  //     console.error("Error selecting items:", err);
+  //   } else {
+  //     console.log("Items:");
+  //     rows.forEach((row) => {
+  //       console.log(row.id, row.name);
+  //     });
+  //   }
+  // });
 
 
-  db.run("UPDATE items SET name = ? WHERE id = ?", ["Updated Item", 1]);
+  // db.run("UPDATE items SET name = ? WHERE id = ?", ["Updated Item", 1]);
 
 
-  db.run("DELETE FROM items WHERE id = ?", [1]);
+  // db.run("DELETE FROM items WHERE id = ?", [1]);
 });
 
 
